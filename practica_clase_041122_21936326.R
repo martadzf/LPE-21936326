600438f0073a2c4d9e32613f457e8173f49e3cd2
Marta Díaz Fuentes <martadiazfuentes@outlook.es>
2022-11-04 19:02:55 +0100

    load data

--- practica_clase_041122_21936326.R
+++ practica_clase_041122_21936326.R
@@ -10,8 +10,19 @@
 # LOADING LIBS ------------------------------------------------------------
 
 if(!require("pacman")) install.packages("pacman")
-p_load(tidyverse, magrittr, janitor, lubridate, tidyr)
+p_load(tidyverse, magrittr, janitor, lubridate, tidyr, jsonlite, httr)
 
 # LOADING DATA ------------------------------------------------------------
+
+url_ <- "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/"
+
+f_raw <- jsonlite::fromJSON(url_)
+df_source <- f_raw$ListaEESSPrecio %>% glimpse()
+janitor::clean_names(df_source) %>% glimpse()
+
+#ver config
+locale() 
+#Cambaimos config
+df1<-df_source %>% janitor::clean_names() %>% type_convert(locale = locale(decimal_mark = ","))
 
 # CREATING NEW COLUMN ------------------------------------------------------------
