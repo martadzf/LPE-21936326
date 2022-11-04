2243ea587bf14e330041e0ff26600c85abc93158
Marta Díaz Fuentes <martadiazfuentes@outlook.es>
2022-11-04 18:56:49 +0100

    created file

---
+++ practica_clase_041122_21936326.R
@@ -0,0 +1,17 @@
+# ID SCRIPT ---------------------------------------------------------------
+
+
+## LENGUAJES DE PROGRAMACIÓN ESTADÍSTICA
+## PROFESOR: CHRISTIAN SUCUZHANAY AREVALO
+## ALUMNO: MARTA DÍAZ, EXP 21936326
+## PRÁCTICA CLASE 04/11/221
+
+
+# LOADING LIBS ------------------------------------------------------------
+
+if(!require("pacman")) install.packages("pacman")
+p_load(tidyverse, magrittr, janitor, lubridate, tidyr)
+
+# LOADING DATA ------------------------------------------------------------
+
+# CREATING NEW COLUMN ------------------------------------------------------------
